0376fe355084f13b197c202976be5e90de366f7c
Andrew Vo <voandrew@berkeley.edu>
2019-12-24 23:32:15 -0800

    Practiced forms and container/component architecture.

--- src/App.js
+++ src/App.js
@@ -3,50 +3,57 @@ import React from "react"
 // import NavBar from "./components/NavBar"
 // import MainContent from "./components/MainContent"
 // import Footer from "./components/Footer"
-import TodoItem from "./components/TodoItem"
+// import TodoItem from "./components/TodoItem"
 // import Joke from "./components/Joke"
 // import Product from "./components/Product"
 // import products from "./data/products"
-import todosData from "./data/todosData"
+// import todosData from "./data/todosData"
+import FormContainer from "./components/FormContainer"
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             todos: todosData
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
 
-    handleClick(id) {
-        this.setState((prevState) => {
-            const updated = prevState.todos.map((task) => {
-                if (task.id === id) {
-                    task.completed = !task.completed
-                }
-                return task
-            })
-            return {
-                todos: updated
-            }
-        })
-    }
+//     handleClick(id) {
+//         this.setState((prevState) => {
+//             const updated = prevState.todos.map((task) => {
+//                 if (task.id === id) {
+//                     task.completed = !task.completed
+//                 }
+//                 return task
+//             })
+//             return {
+//                 todos: updated
+//             }
+//         })
+//     }
 
-    render() {
-        const tasks = this.state.todos.map((task) =>
-            <TodoItem
-                key={task.id}
-                item={task}
-                handleChange={this.handleClick}
-            />)
+//     render() {
+//         const tasks = this.state.todos.map((task) =>
+//             <TodoItem
+//                 key={task.id}
+//                 item={task}
+//                 handleChange={this.handleClick}
+//             />)
 
-        return (
-            <div>
-                {tasks}
-            </div>
-        )
-    }
+//         return (
+//             <div>
+//                 {tasks}
+//             </div>
+//         )
+//     }
+// }
+
+
+function App() {
+    return (
+        <FormContainer />
+    )
 }
-
 
 export default App
