ca3acffb22e205d1f6d5687fd68ae10a51622c51
Andrew Vo <voandrew@berkeley.edu>
2020-01-06 21:59:08 -0800

    Practiced using CSS Grid.

--- src/components/DS.js
+++ src/components/DS.js
@@ -9,6 +9,7 @@ export default class DS extends Component {
             array: []
         }
         this.generateArray = this.generateArray.bind(this);
+        this.unsortArray = this.unsortArray.bind(this);
     }
 
     componentDidMount() {
@@ -23,18 +24,33 @@ export default class DS extends Component {
         this.setState({array: temp});
     }
 
+    unsortArray() {
+        let copy = this.state.array
+        for (let i = 0; i < 10; i += 1) {
+            let index = Math.floor(Math.random() * 10);
+            let temp = copy[i];
+            copy[i] = copy[index];
+            copy[index] = temp;
+        }
+        this.setState({array: copy});
+    }
+
     render() {
         let copy = this.state.array.map(elem => (
-            <div key={elem} className="array-box">
-                <div className="element">
-                    {elem}
-                </div>
+            <div key={elem} className="element">
+                {elem}
             </div>
         ));
         return (
-            <div className="array">
-                {copy}
+            <div>
+                <div className="array">
+                    <div className="array-box">
+                        {copy}
+                     </div>
+                </div> 
+                <button className="button" onClick={this.unsortArray}>Mix the array!</button>
             </div>
+               
         )
     }
 }
